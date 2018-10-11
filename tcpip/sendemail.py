f842a2ebd0c0932f8ff28f2e47070e5629b3d776
赵康 <kang@wlwbshop.com>
2018-10-11 14:54:16 +0800

    commit

---
+++ tcpip/sendemail.py
@@ -0,0 +1,26 @@
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+import smtplib
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr((Header(name, 'utf-8').encode(), addr))
+
+from_addr = 'clunyes@126.com'
+password = 'kisszk1234567'
+# 输入收件人地址:
+to_addr = 'kang@wlwbshop.com'
+# 输入SMTP服务器地址:
+smtp_server = 'smtp.126.com'
+
+msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
+msg['To'] = _format_addr('管理员 <%s>' % to_addr)
+msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
+
+server = smtplib.SMTP(smtp_server, 25)
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
